ef48d3ac4786410e7ab529907306698d44bd5dcd
CalviceGaming <diogo.pereira.d24@gmail.com>
2024-11-11 14:24:21 +0000

    Small Change to the placeholder

--- app/src/main/java/pt/iade/games/detectiveribbit/ui/components/CollectiblePlaceHolder.kt
+++ app/src/main/java/pt/iade/games/detectiveribbit/ui/components/CollectiblePlaceHolder.kt
@@ -2,14 +2,19 @@ package pt.iade.games.detectiveribbit.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pt.iade.games.detectiveribbit.models.Collectible
 import java.net.URI
 
@@ -17,7 +22,10 @@ import java.net.URI
 fun CollectiblePlaceHolder(
     collectible: Collectible
 ){
-    Card{
+    Card(
+        onClick = {
+        }
+    ){
         Column (modifier = Modifier.padding(5.dp)){
             Button(
                 onClick = {}
@@ -29,11 +37,39 @@ fun CollectiblePlaceHolder(
 @Preview
 @Composable
 fun CollectiblePlaceHolderPreview(){
-    CollectiblePlaceHolder(collectible = Collectible(
+    CollectibleInfo(collectible = Collectible(
         id = 1,
         name = "Statue",
         image = URI.create("https://i.ytimg.com/vi/2S0XDjzfgO8/maxresdefault.jpg"),
-        description = "Statue of Ribbit",
+        description = "Statue of Ribbit, " +
+                "Found in the mafia Stackhouse.",
         placeholderSize = 1
     ))
+}
+
+@Composable
+fun CollectibleInfo(
+    collectible: Collectible
+){
+    Card() {
+        Column(
+            modifier = Modifier.padding(5.dp)
+        ){
+            Row {
+                Text(
+                    text = collectible.name,
+                    modifier = Modifier.padding(bottom = 10.dp, top = 10.dp)
+                        .weight(weight = 0.70f),
+                    fontSize = 30.sp
+                )
+                Button(onClick = {},
+                    ) { Text(text = "Close",
+                        fontSize =10.sp,
+                    )}
+            }
+            Text(text = collectible.description,
+                minLines = 2,
+                maxLines = 2)
+        }
+    }
 }
